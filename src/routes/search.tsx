51cff8a2446870ed1182c66e3fda194278da5ea8
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-06-20 18:35:13 +0500

    feat: integrate user quiz results into Tabs and Search components to display correct answers

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useSnapshot } from "valtio";
 import { Drawer } from "vaul";
 import { Coin } from "~/components/Coin";
+import { Complete } from "~/components/Icons/Complete";
 import { QuizDrawer } from "~/components/QuizDrawer";
 import { Warning } from "~/components/Warning";
 import { useUser } from "~/hooks/useUser";
@@ -23,6 +24,8 @@ function SearchPage() {
   const [search, setSearch] = useState("");
 
   const { data: quizes } = useQuery(trpc.quizzes.getAll.queryOptions());
+
+  const { data: userQuizCoins } = useQuery(trpc.main.getUserResults.queryOptions());
 
   const filteredQuizes =
     search.trim() !== ""
@@ -97,93 +100,112 @@ function SearchPage() {
         {search.trim() !== "" ? (
           filteredQuizes && filteredQuizes.length > 0 ? (
             <div className="mt-3 grid grid-cols-2 gap-4 px-4">
-              {filteredQuizes.map((quiz) => (
-                <Drawer.Root
-                  key={quiz.id}
-                  open={openQuizId === quiz.id}
-                  onOpenChange={(open) => setOpenQuizId(open ? quiz.id : null)}
-                >
-                  <Drawer.Trigger asChild>
-                    <div className="cursor-pointer">
-                      <div
-                        style={{
-                          borderTop: "4px solid white",
-                          borderLeft: "4px solid white",
-                          borderRight: "4px solid #293133",
-                          borderBottom: "4px solid #293133",
-                        }}
-                        className="relative h-54 w-full"
-                      >
-                        <div className="flex max-h-6 w-full items-center bg-[#010089]">
-                          <p className="px-2 py-2 text-xs uppercase">
-                            {quiz.categories.length > 0
-                              ? quiz.categories[0].name.substring(0, 18)
-                              : ""}
-                          </p>
-                        </div>
-                        <img
-                          className="h-46 w-full object-cover"
-                          src={quiz.imageUrl || "/placeholder.png"}
-                          alt={quiz.title}
-                          width={1000}
-                          height={100}
-                        />
-                        <div className="absolute inset-0 bg-[linear-gradient(to_top,rgba(0,0,0,0.8),transparent)]" />
-                        <div className="absolute bottom-2 left-2">
-                          {quiz.isNew && (
-                            <h3 className="text-md font-medium text-[#F97316] uppercase">
-                              Новинка
-                            </h3>
-                          )}
-                          <h3 className="text-md mt-1 w-40 break-words uppercase">
-                            {quiz.title}
-                          </h3>
-                        </div>
-                      </div>
-                    </div>
-                  </Drawer.Trigger>
-                  <Drawer.Portal>
-                    <Drawer.Overlay className="fixed inset-0 z-40 bg-black/50" />
-                    <Drawer.Content className="fixed right-0 bottom-0 left-0 z-50 mt-24 flex h-screen flex-col rounded-t-[10px] bg-[#212121]">
-                      <div className="5 flex items-center justify-between">
-                        <svg
-                          onClick={() => setShowWarning(true)}
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M7 19H5V17H7V19ZM19 19H17V17H19V19ZM9 15V17H7V15H9ZM17 17H15V15H17V17ZM11 15H9V13H11V15ZM15 15H13V13H15V15ZM13 13H11V11H13V13ZM11 11H9V9H11V11ZM15 11H13V9H15V11ZM9 9H7V7H9V9ZM17 9H15V7H17V9ZM7 7H5V5H7V7ZM19 7H17V5H19V7Z"
-                            fill="white"
-                          />
-                        </svg>
-                        <Warning
-                          onConfirm={() => setOpenQuizId(null)}
-                          onCancel={() => {}}
-                        />
-                        <div className="flex items-center gap-2">
-                          <Coin />
-                          {user?.totalScore}
-                        </div>
-                      </div>
-                      <div className="flex-1 overflow-y-auto">
-                        <QuizDrawer
-                          quizId={quiz.id}
-                          onClose={() => {
-                            setOpenQuizId(null);
-                            if (!user?.isMember) {
-                              setIsSubscribed(false);
-                              console.log("setIsSubscribed(false)");
-                            }
+              {filteredQuizes.map((quiz) => {
+                const quizResult = userQuizCoins?.find(
+                  (result) => result.quizId === quiz.id,
+                );
+
+                return (
+                  <Drawer.Root
+                    key={quiz.id}
+                    open={openQuizId === quiz.id}
+                    onOpenChange={(open) => setOpenQuizId(open ? quiz.id : null)}
+                  >
+                    <Drawer.Trigger asChild>
+                      <div className="cursor-pointer">
+                        <div
+                          style={{
+                            borderTop: "4px solid white",
+                            borderLeft: "4px solid white",
+                            borderRight: "4px solid #293133",
+                            borderBottom: "4px solid #293133",
                           }}
-                        />
+                          className="relative h-54 w-full"
+                        >
+                          <div className="flex max-h-6 w-full items-center bg-[#010089]">
+                            <p className="px-2 py-2 text-xs uppercase">
+                              {quiz.categories.length > 0
+                                ? quiz.categories[0].name.substring(0, 18)
+                                : ""}
+                            </p>
+                          </div>
+                          <img
+                            className="h-46 w-full object-cover"
+                            src={quiz.imageUrl || "/placeholder.png"}
+                            alt={quiz.title}
+                            width={1000}
+                            height={100}
+                          />
+                          <div className="absolute inset-0 bg-[linear-gradient(to_top,rgba(0,0,0,0.8),transparent)]" />
+                          <div className="absolute bottom-2 left-2">
+                            {quiz.isNew && (
+                              <h3 className="text-md font-medium text-[#F97316] uppercase">
+                                Новинка
+                              </h3>
+                            )}
+                            <h3 className="text-md mt-1 w-40 break-words uppercase">
+                              {quiz.title}
+                            </h3>
+                          </div>
+                          {quizResult?.correctAnswers &&
+                          quizResult?.correctAnswers > 0 ? (
+                            <div className="absolute top-1/2 left-1/2 flex min-h-[85px] min-w-[85px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-1 bg-[#28282899] p-4">
+                              <div className="flex items-center justify-center bg-[#25CE16] p-2">
+                                <Complete />
+                              </div>
+
+                              <div>Пройдено</div>
+                              {`${quizResult.correctAnswers}/${quiz.questions.length}`}
+                            </div>
+                          ) : (
+                            ""
+                          )}
+                        </div>
                       </div>
-                    </Drawer.Content>
-                  </Drawer.Portal>
-                </Drawer.Root>
-              ))}
+                    </Drawer.Trigger>
+                    <Drawer.Portal>
+                      <Drawer.Overlay className="fixed inset-0 z-40 bg-black/50" />
+                      <Drawer.Content className="fixed right-0 bottom-0 left-0 z-50 mt-24 flex h-screen flex-col rounded-t-[10px] bg-[#212121]">
+                        <div className="5 flex items-center justify-between">
+                          <svg
+                            onClick={() => setShowWarning(true)}
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M7 19H5V17H7V19ZM19 19H17V17H19V19ZM9 15V17H7V15H9ZM17 17H15V15H17V17ZM11 15H9V13H11V15ZM15 15H13V13H15V15ZM13 13H11V11H13V13ZM11 11H9V9H11V11ZM15 11H13V9H15V11ZM9 9H7V7H9V9ZM17 9H15V7H17V9ZM7 7H5V5H7V7ZM19 7H17V5H19V7Z"
+                              fill="white"
+                            />
+                          </svg>
+                          <Warning
+                            onConfirm={() => setOpenQuizId(null)}
+                            onCancel={() => {}}
+                          />
+                          <div className="flex items-center gap-2">
+                            <Coin />
+                            {user?.totalScore}
+                          </div>
+                        </div>
+                        <div className="flex-1 overflow-y-auto">
+                          <QuizDrawer
+                            quizId={quiz.id}
+                            onClose={() => {
+                              setOpenQuizId(null);
+                              if (!user?.isMember) {
+                                setIsSubscribed(false);
+                                console.log("setIsSubscribed(false)");
+                              }
+                            }}
+                          />
+                        </div>
+                      </Drawer.Content>
+                    </Drawer.Portal>
+                  </Drawer.Root>
+                );
+              })}
             </div>
           ) : (
             <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-2">
