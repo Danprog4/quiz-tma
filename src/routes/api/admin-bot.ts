8cc17264a198bbe6aa0a5e7bce2393e921efc8e1
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-06-21 15:19:58 +0500

    feat: integrate @grammyjs/conversations for enhanced admin bot functionality and add news creation command

--- src/routes/api/admin-bot.ts
+++ src/routes/api/admin-bot.ts
@@ -1,10 +1,17 @@
+import {
+  Conversation,
+  conversations,
+  createConversation,
+  type ConversationFlavor,
+} from "@grammyjs/conversations";
 import { createAPIFileRoute } from "@tanstack/react-start/api";
-import { Bot, webhookCallback } from "grammy";
+import { Bot, Context, webhookCallback } from "grammy";
 import { db } from "~/lib/db";
 import { newsTable } from "~/lib/db/schema";
 import { getIsAdmin } from "~/lib/utils/getIsAdmin";
 
-const bot = new Bot(process.env.ADMIN_BOT_TOKEN as string);
+const bot = new Bot<ConversationFlavor<Context>>(process.env.ADMIN_BOT_TOKEN as string);
+bot.use(conversations());
 
 bot.command("start", (ctx) => {
   if (!getIsAdmin(Number(ctx.from?.id))) {
@@ -15,7 +22,7 @@ bot.command("start", (ctx) => {
   ctx.reply("Привет, Админ!");
 });
 
-bot.command("create-news", (ctx) => {
+bot.command("createnews", (ctx) => {
   if (!getIsAdmin(Number(ctx.from?.id))) {
     ctx.reply("У тебя нет доступа к этому боту");
     return;
@@ -37,11 +44,33 @@ bot.command("create-news", (ctx) => {
   });
 });
 
+async function createNews(conversation: Conversation, ctx: Context) {
+  if (!getIsAdmin(Number(ctx.from?.id))) {
+    ctx.reply("У тебя нет доступа к этому боту");
+    return;
+  }
+
+  await ctx.reply(
+    "Введите текст новости, ссылку и ссылку на изображение. Формат: блаблабла,ссылка,ссылка",
+  );
+
+  const { message } = await conversation.waitFor("message:text");
+  const [text, link, imageUrl] = message.text.split(",");
+  try {
+    await db.insert(newsTable).values({ text, link, imageUrl });
+    await ctx.reply("Новость создана успешно");
+  } catch (error) {
+    await ctx.reply("Не удалось создать новость");
+  }
+}
+
 const update = webhookCallback(bot, "std/http");
 
 const handleUpdate = async (request: Request) => {
   return await update(request);
 };
+
+bot.use(createConversation(createNews, "createNews"));
 
 export const APIRoute = createAPIFileRoute("/api/admin-bot")({
   GET: async ({ request }) => handleUpdate(request),
