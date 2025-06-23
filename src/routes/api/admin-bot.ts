aa3166177a3572acfdaf062d0fd43c9e4bc69300
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-06-23 12:56:33 +0500

    feat: implement quiz update functionality in admin bot, allowing admins to modify quiz details interactively

--- src/routes/api/admin-bot.ts
+++ src/routes/api/admin-bot.ts
@@ -5,6 +5,7 @@ import {
   type ConversationFlavor,
 } from "@grammyjs/conversations";
 import { createAPIFileRoute } from "@tanstack/react-start/api";
+import { eq } from "drizzle-orm";
 import { Bot, Context, webhookCallback } from "grammy";
 import { db } from "~/lib/db";
 import { answersTable, newsTable, questionsTable, quizzesTable } from "~/lib/db/schema";
@@ -254,6 +255,179 @@ bot.command("create_quiz", async (ctx) => {
   }
   await ctx.conversation.enter("create_quiz");
 });
+
+async function updateQuiz(conversation: Conversation, ctx: Context) {
+  if (!getIsAdmin(Number(ctx.from?.id))) {
+    ctx.reply("У тебя нет доступа к этому боту");
+    return;
+  }
+
+  await ctx.reply("Введите ID или название квиза, который хотите обновить:");
+
+  let quiz;
+  while (true) {
+    const { message } = await conversation.waitFor("message:text");
+    const response = message.text;
+
+    quiz = await db.query.quizzesTable.findFirst({
+      where: eq(quizzesTable.id, Number(response)) || eq(quizzesTable.title, response),
+    });
+
+    if (!quiz) {
+      await ctx.reply("Квиз не найден, попробуйте ввести ID или название еще раз");
+      continue;
+    } else {
+      break;
+    }
+  }
+
+  while (true) {
+    await ctx.reply(
+      "Введите что хотите обновить. Например, название, описание, ссылка на изображение, популярность, новый, максимальный балл, имя коллаборатора, логотип коллаборатора, ссылка коллаборатора. Можно ввести только одно поле",
+    );
+
+    const { message } = await conversation.waitFor("message:text");
+    const update = message.text;
+
+    switch (update) {
+      case "название":
+        await ctx.reply("Введите новое название квиза");
+        const { message: newTitle } = await conversation.waitFor("message:text");
+        await db
+          .update(quizzesTable)
+          .set({ title: newTitle.text })
+          .where(eq(quizzesTable.id, quiz.id));
+        await ctx.reply(
+          "Название квиза обновлено. Напишите 'да' если хотите обновить что-то ещё или 'нет' если хотите закончить",
+        );
+        const { message: titleContinue } = await conversation.waitFor("message:text");
+        if (titleContinue.text.toLowerCase() !== "да") {
+          break;
+        }
+      case "описание":
+        await ctx.reply("Введите новое описание квиза");
+        const { message: newDescription } = await conversation.waitFor("message:text");
+        await db
+          .update(quizzesTable)
+          .set({ description: newDescription.text })
+          .where(eq(quizzesTable.id, quiz.id));
+        await ctx.reply(
+          "Описание квиза обновлено. Напишите 'да' если хотите обновить что-то ещё или 'нет' если хотите закончить",
+        );
+        const { message: descriptionContinue } =
+          await conversation.waitFor("message:text");
+        if (descriptionContinue.text.toLowerCase() !== "да") {
+          break;
+        }
+      case "ссылка на изображение":
+        await ctx.reply("Введите новую ссылку на изображение");
+        const { message: newImageUrl } = await conversation.waitFor("message:text");
+        await db
+          .update(quizzesTable)
+          .set({ imageUrl: newImageUrl.text })
+          .where(eq(quizzesTable.id, quiz.id));
+        await ctx.reply(
+          "Ссылка на изображение квиза обновлена. Напишите 'да' если хотите обновить что-то ещё или 'нет' если хотите закончить",
+        );
+        const { message: imageContinue } = await conversation.waitFor("message:text");
+        if (imageContinue.text.toLowerCase() !== "да") {
+          break;
+        }
+      case "популярность":
+        await ctx.reply("Введите новую популярность квиза");
+        const { message: newIsPopular } = await conversation.waitFor("message:text");
+        await db
+          .update(quizzesTable)
+          .set({ isPopular: newIsPopular.text === "true" })
+          .where(eq(quizzesTable.id, quiz.id));
+        await ctx.reply(
+          "Популярность квиза обновлена. Напишите 'да' если хотите обновить что-то ещё или 'нет' если хотите закончить",
+        );
+        const { message: popularContinue } = await conversation.waitFor("message:text");
+        if (popularContinue.text.toLowerCase() !== "да") {
+          break;
+        }
+      case "новый":
+        await ctx.reply("Введите новую новизну квиза");
+        const { message: newIsNew } = await conversation.waitFor("message:text");
+        await db
+          .update(quizzesTable)
+          .set({ isNew: newIsNew.text === "true" })
+          .where(eq(quizzesTable.id, quiz.id));
+        await ctx.reply(
+          "Новизна квиза обновлена. Напишите 'да' если хотите обновить что-то ещё или 'нет' если хотите закончить",
+        );
+        const { message: newIsNewContinue } = await conversation.waitFor("message:text");
+        if (newIsNewContinue.text.toLowerCase() !== "да") {
+          break;
+        }
+      case "максимальный балл":
+        await ctx.reply("Введите новый максимальный балл");
+        const { message: newMaxScore } = await conversation.waitFor("message:text");
+        await db
+          .update(quizzesTable)
+          .set({ maxScore: parseInt(newMaxScore.text) })
+          .where(eq(quizzesTable.id, quiz.id));
+        await ctx.reply(
+          "Максимальный балл квиза обновлен. Напишите 'да' если хотите обновить что-то ещё или 'нет' если хотите закончить",
+        );
+        const { message: maxScoreContinue } = await conversation.waitFor("message:text");
+        if (maxScoreContinue.text.toLowerCase() !== "да") {
+          break;
+        }
+      case "имя коллаборатора":
+        await ctx.reply("Введите новое имя коллаборатора");
+        const { message: newCollaboratorName } =
+          await conversation.waitFor("message:text");
+        await db
+          .update(quizzesTable)
+          .set({ collaboratorName: newCollaboratorName.text })
+          .where(eq(quizzesTable.id, quiz.id));
+        await ctx.reply(
+          "Имя коллаборатора квиза обновлено. Напишите 'да' если хотите обновить что-то ещё или 'нет' если хотите закончить",
+        );
+        const { message: collaboratorNameContinue } =
+          await conversation.waitFor("message:text");
+        if (collaboratorNameContinue.text.toLowerCase() !== "да") {
+          break;
+        }
+      case "логотип коллаборатора":
+        await ctx.reply("Введите новый логотип коллаборатора");
+        const { message: newCollaboratorLogo } =
+          await conversation.waitFor("message:text");
+        await db
+          .update(quizzesTable)
+          .set({ collaboratorLogo: newCollaboratorLogo.text })
+          .where(eq(quizzesTable.id, quiz.id));
+        await ctx.reply(
+          "Логотип коллаборатора квиза обновлен. Напишите 'да' если хотите обновить что-то ещё или 'нет' если хотите закончить",
+        );
+        const { message: collaboratorLogoContinue } =
+          await conversation.waitFor("message:text");
+        if (collaboratorLogoContinue.text.toLowerCase() !== "да") {
+          break;
+        }
+      case "ссылка коллаборатора":
+        await ctx.reply("Введите новую ссылку коллаборатора");
+        const { message: newCollaboratorLink } =
+          await conversation.waitFor("message:text");
+        await db
+          .update(quizzesTable)
+          .set({ collaboratorLink: newCollaboratorLink.text })
+          .where(eq(quizzesTable.id, quiz.id));
+        await ctx.reply(
+          "Ссылка коллаборатора квиза обновлена. Напишите 'да' если хотите обновить что-то ещё или 'нет' если хотите закончить",
+        );
+        const { message: collaboratorLinkContinue } =
+          await conversation.waitFor("message:text");
+        if (collaboratorLinkContinue.text.toLowerCase() !== "да") {
+          break;
+        }
+      default:
+        await ctx.reply("Неверный формат, попробуйте снова");
+    }
+  }
+}
 
 const update = webhookCallback(bot, "std/http");
 
